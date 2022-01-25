3dc0847a67bfb98f3b6735b83c09ae5da7c5579f
GiuliaSossai <giulia.sossai@yahoo.it>
2022-01-25 16:29:19 +0100

    init project

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header class="container my-4">
+    <ul class="nav nav-pills">
+    <li class="nav-item">
+        <a class="nav-link {{ (Route::currentRouteName() === 'home' ) ? 'active' : '' }}" href="{{ route('home') }}">Home</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link {{ (Route::currentRouteName() === 'travel' ) ? 'active' : '' }}" href="{{ route('travel') }}">Travel</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link {{ (Route::currentRouteName() === 'contacts' ) ? 'active' : '' }}" href="{{ route('contacts') }}">Contacts</a>
+    </li>
+    </ul>
+</header>
---
+++ resources/views/travel.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        <h1>Travel</h1>
+    </div>
+    
+    
+@endsection
